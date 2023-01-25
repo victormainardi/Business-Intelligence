3e73f8d489ad8549388fb784f581d8431f0518cb
Victor Mainardi <victor.emf_@hotmail.com>
2023-01-25 22:58:45 +0000

    fix

--- Atividade 3/consultas/consulta_ROLLUP.sql
+++ Atividade 3/consultas/consulta_ROLLUP.sql
@@ -9,15 +9,3 @@ SELECT COALESCE(lojas.nome,'Total') as Loja,
 FROM vendas
 JOIN lojas ON vendas.id_loja = lojas.id
 GROUP BY ROLLUP (lojas.nome,lojas.estado);
-
-
-/*
-SELECT nome, 
-       estado, 
-       SUM(valor) as total_vendas
-FROM Vendas
-JOIN Lojas ON Vendas.id_loja = Lojas.id
-GROUP BY nome, estado
-ORDER BY estado, nome;
-
-*/
